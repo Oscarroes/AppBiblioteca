ea5a161f26070e2e360d3eb5c6c4fc3578a02709
Oscar <151192670+Oscarroes@users.noreply.github.com>
2024-03-13 23:47:29 +0100

    V5 - Ventana agregar libro

--- MiBiblioteca/vista.py
+++ MiBiblioteca/vista.py
@@ -1,5 +1,7 @@
 from customtkinter import *
 import tkinter as tk
+# import ttkbootstrap as ttk
+# from ttkbootstrap.constants import *
 from tkinter import filedialog
 from PIL import Image
 import os
@@ -23,10 +25,6 @@ class Vista:
         self.imagenPath = tk.StringVar()
         self.imagenSalir = Image.open("img/iconoEncendido.ico")
 
-        # self.etiquetaTitulo = CTkLabel(master=self.ventana,text="Titulo")
-        # self.etiquetaTitulo.grid(row=0, column=0, padx=10, pady=5)
-        # self.entradaTitulo = CTkEntry(master=self.ventana)
-        # self.entradaTitulo.grid(row=0, column=1, padx=10, pady=5)
 
         # self.etiquetaAutor = CTkLabel(master=self.ventana,text="Autor")
         # self.etiquetaAutor.grid(row=1, column=0, padx=10, pady=5)
@@ -52,7 +50,7 @@ class Vista:
         #BOTÓN DE AGREGAR NUEVO LIBRO
         self.boton = CTkButton(master=self.frameMenu,
                                text="Nuevo libro",
-                                command=self.guardarInfo,
+                                command=self.abrirVentanaInsertar,
                                 text_color="#F6FFF9",
                                 height=40,
                                 width=210,
@@ -188,6 +186,179 @@ class Vista:
 
     
     #--------------------FUNCIONES---------------------------------------------------------
+        
+    #--------------------VENTANA INSERTAR LIBRO---------------------------------------------------------
+        
+    def abrirVentanaInsertar(self):
+        
+        ventanaInsertar = tk.Toplevel(self.ventana)
+        ventanaInsertar.geometry("896x768")
+        ventanaInsertar.config(bg="#2E4C39")
+        ventanaInsertar.title('Añade un nuevo libro')
+        ventanaInsertar.lift()
+
+        def posicionarVentana():
+            ventanaInsertar.lift()
+
+        self.frameSeparador = CTkFrame(master=ventanaInsertar, fg_color="#2E4C39", height=10)
+        self.frameSeparador.grid(row=0, column=0, padx=10, pady=5)
+
+        #ENTRADA TÍTULO--------------------------------------------------
+        self.etiquetaTitulo = CTkLabel(master=ventanaInsertar,
+                                       text="Título: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaTitulo.grid(row=1, column=0, padx=10, pady=5)
+        self.entradaTitulo = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200)
+        self.entradaTitulo.grid(row=1, column=1, padx=10, pady=5)
+
+        #ENTRADA AUTOR-------------------------------------------------------
+        self.etiquetaAutor = CTkLabel(master=ventanaInsertar,
+                                       text="Autor: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaAutor.grid(row=2, column=0, padx=10, pady=5)
+        self.entradaAutor = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200)
+        self.entradaAutor.grid(row=2, column=1, padx=10, pady=0)
+
+        #ENTRADA GÉNERO-------------------------------------------------------
+
+        self.etiquetaGenero = CTkLabel(master=ventanaInsertar,
+                                       text="Género: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaGenero.grid(row=3, column=0, padx=10, pady=5)
+        self.entradaGenero = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200)
+        self.entradaGenero.grid(row=3, column=1, padx=10, pady=5)
+
+        #ENTRADA PÁGINAS-------------------------------------------------------
+
+        self.etiquetaPaginas = CTkLabel(master=ventanaInsertar,
+                                       text="Número de páginas: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaPaginas.grid(row=4, column=0, padx=10, pady=5)
+        self.entradaPaginas = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200)
+        self.entradaPaginas.grid(row=4, column=1, padx=10, pady=5)
+
+        #ENTRADA FECHA DE PUBLICACIÓN-------------------------------------------------------
+
+        self.etiquetaFecha = CTkLabel(master=ventanaInsertar,
+                                       text="Fecha de publicación: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaFecha.grid(row=5, column=0, padx=10, pady=5)
+        self.entradaFecha = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200,
+                                       placeholder_text="DD/MM/AAAA",
+                                       placeholder_text_color="#B8B8B8")
+        self.entradaFecha.grid(row=5, column=1, padx=10, pady=5)
+
+        #ENTRADA EDITORIAL-------------------------------------------------------
+
+        self.etiquetaGenero = CTkLabel(master=ventanaInsertar,
+                                       text="Género: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaGenero.grid(row=6, column=0, padx=10, pady=5)
+        self.entradaGenero = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200)
+        self.entradaGenero.grid(row=6, column=1, padx=10, pady=5)
+
+        #ENTRADA ISBN-------------------------------------------------------
+
+        self.etiquetaISBN = CTkLabel(master=ventanaInsertar,
+                                       text="ISBN: ",
+                                       justify="left",
+                                       anchor="w",
+                                       width=200,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaISBN.grid(row=7, column=0, padx=10, pady=5)
+        self.entradaISBN = CTkEntry(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       width=200)
+        self.entradaISBN.grid(row=7, column=1, padx=10, pady=5)
+
+        #ENTRADA PORTADA-------------------------------------------------------
+
+        self.etiquetaPortada = CTkLabel(master=ventanaInsertar,
+                                        text="Portada: ",
+                                        justify="left",
+                                        anchor="w",
+                                        width=200,
+                                        font=("Roboto", 18, "bold"),
+                                        text_color="#F6FFF9")
+        self.etiquetaPortada.grid(row=8, column=0, padx=10, pady=5)
+        self.entradaPortada = CTkEntry(master=ventanaInsertar,
+                                        textvariable=self.imagenPath,
+                                        state="readonly",
+                                        font=("Roboto", 12, "bold"),
+                                        width=200)
+        self.entradaPortada.grid(row=8, column=2, padx=10, pady=5)
+
+        #BOTÓN DE EXPLORACIÓN
+        self.botonExplorador = CTkButton(master=ventanaInsertar,text="Abrir explorador",
+                                        command=lambda: [self.abrirExplorador(),posicionarVentana()],
+                                        width=200,
+                                        text_color="#F6FFF9",
+                                        font=("Roboto", 18, "bold"),
+                                        fg_color="#75CB92",
+                                        hover_color="#0AE7E0",
+                                        border_color="#636E67",
+                                        border_width=3)
+        #     # self.botonExplorador.place(relx=0.8, rely=0.35, anchor="center")
+        self.botonExplorador.grid(row=8, column=1, padx=10, pady=5)
+
+        #ENTRADA SINOPSIS-------------------------------------------------------
+
+        self.etiquetaSinopsis = CTkLabel(master=ventanaInsertar,
+                                       text="Sinopsis: ",
+                                       justify="left",
+                                       anchor="nw",
+                                       width=200,
+                                       height=250,
+                                       font=("Roboto", 18, "bold"),
+                                       text_color="#F6FFF9")
+        self.etiquetaSinopsis.grid(row=9, column=0, padx=10, pady=5)
+        self.entradaSinopsis = CTkTextbox(master=ventanaInsertar,
+                                       font=("Roboto", 12, "bold"),
+                                       scrollbar_button_color="#75CB92",
+                                       width=430,
+                                       height=250)
+        self.entradaSinopsis.grid(row=9, column=1, columnspan=2, padx=10, pady=5)
+
+
+
+
     
     def guardarInfo(self):
         titulo = self.entradaTitulo.get()
@@ -209,6 +380,7 @@ class Vista:
 
     # Función para abrir un explorador de archivos y seleccionar una imagen
     def abrirExplorador(self):
+        
         rutaImagen = filedialog.askopenfilename(initialdir=os.getcwd(), title="Seleccionar imagen", filetypes=[("Archivos de imagen", "*.jpg;*.jpeg;*.png")])
         if rutaImagen:
             self.imagenPath.set(rutaImagen)
@@ -247,4 +419,15 @@ class Vista:
 
         # Guardar la referencia a la imagen para evitar que sea eliminada por el recolector de basura
         self.lienzoLibro.imagen = imagen
+
+    #CREAR FONDO DE IMAGEN Y LUEGO PONER UN FRAME ENCIMA Y LOS WIDGETS EN EL FRAME ¿FUNCIONA?:
+        # fondo = tk.PhotoImage(file="fondoDragon.png")
+        # canvas = tk.Canvas(raiz, width=fondo.width(), height=fondo.height())
+        # canvas.pack()
+        # canvas.create_image(0,0,anchor=tk.NW, image=fondo)
+
+        # usuario = tk.Label(canvas,text="Usuario",anchor="w",justify="left",width=22,font=("Verdana",10),bg="#B4B2B2")
+        # canvas.create_window(50, 500, anchor=tk.NW, window=usuario)
+        # usuarioEntry = tk.Entry(canvas, width=22,font=("Verdana",10),bg="#E7E9E9")
+        # canvas.create_window(50, 525, anchor=tk.NW, window=usuarioEntry)
         
